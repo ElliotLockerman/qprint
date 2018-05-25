b741437cc06e539c7043208ba97f954ad5e36042
Elliot Lockerman <elliot@lockerman.info>
2018-05-24 20:27:19 -0400

    Printing contents of iterables (e.g., vector, map, etc)

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,14 +1,26 @@
 
+#include <vector>
+#include <unordered_map>
 
-#include "qprint.hpp"
+#include "../qprint.hpp"
 
 
 int main() {
+
     qprint("");
     qprint("test a\n");
     qprint("ten: {}\n", 10);
 
-    auto str = qformat("ten: {}, twenty: {}\n", 10, 20);
+    int ten = 10;
+    const char* twenty = "twenty";
+    auto str = qformat("ten: {}, twenty: {}\n", ten, twenty);
     qprint("{}", str);
+    qprint("{}", "string literal\n");
+
+    std::vector<int> v{1,2,3,4};
+    qprint("{}\n", v);
+
+    std::unordered_map<char, int> map{{'a', 1}, {'b', 2}, {'c', 3}};
+    qprint("{}\n", map);
 }
 
